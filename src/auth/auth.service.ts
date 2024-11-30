35b556c76160b8fe8f62229336b572e746226e84
Lara Rodrigues <lararods73@gmail.com>
2024-11-30 16:22:52 -0300

    Finishing authentication
    adding jwt tokens and strategy

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,11 +4,30 @@ import { PrismadbService } from "src/prismadb/prismadb.service";
 import { AuthDto } from "./dto";
 import * as argon from 'argon2';
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+import { JwtService } from "@nestjs/jwt";
+import { ConfigService } from "@nestjs/config";
 
 //injection = allows to create a instance without declaring. instead it does via dependency injection
 @Injectable()
 export class AuthService {
-    constructor(private prisma: PrismadbService) { }
+    constructor(private prisma: PrismadbService, private jwt: JwtService, private config: ConfigService) { }
+
+   async signToken(userId: number, email: string): Promise<{access_token: string}>{
+        const payload = {
+            sub: userId,
+            email
+        }
+        const secret = this.config.get('JWT_SECRET');
+        
+        const token = await this.jwt.signAsync(payload, {
+            expiresIn: '15m',
+            secret: secret
+        });
+
+        return {
+            access_token: token
+        }
+    }
 
     async signin(dto: AuthDto) {
         //find user by email or throw exception
@@ -24,9 +43,9 @@ export class AuthService {
         const pwMatches = await argon.verify(user.hash, dto.password);
 
         if(!pwMatches) throw new ForbiddenException("Incorrect credentials");
-        //return user
-        delete user.hash;
-        return user;
+
+        //return token
+        return this.signToken(user.id, user.email);
     }
 
     async signup(dto: AuthDto) {
