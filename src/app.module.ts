443f2cdb517b40045f4b4320117a8a71fec1a6c1
Lara Rodrigues <lararods73@gmail.com>
2024-11-30 15:33:07 -0300

    initial auth configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { PrismadbModule } from './prismadb/prismadb.module';
 
+
 //Module: Container for diffrent parts of an application(Similiar to a package maybe?)
 //@Module: Decorator. A modul can import other modules. Decorator = Bean
 //terminal: nest g module_name to automatically generate a module
 @Module({
-  imports: [AuthModule, UserModule, BookmarkModule, PrismadbModule],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+  }), AuthModule, UserModule, BookmarkModule, PrismadbModule],
   controllers: [],
   providers: [],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,17 +1,59 @@
-import { Injectable } from "@nestjs/common";
-import {User, Bookmark} from '@prisma/client'; //types created automatically by prisma
+import { ForbiddenException, Injectable } from "@nestjs/common";
+import { User, Bookmark } from '@prisma/client'; //types created automatically by prisma
 import { PrismadbService } from "src/prismadb/prismadb.service";
+import { AuthDto } from "./dto";
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 
 //injection = allows to create a instance without declaring. instead it does via dependency injection
 @Injectable()
-export class AuthService{
-    constructor(private prisma: PrismadbService){}
+export class AuthService {
+    constructor(private prisma: PrismadbService) { }
 
-    signin(){
+    async signin(dto: AuthDto) {
+        //find user by email or throw exception
+        const user = await this.prisma.user.findUnique({
+            where: {
+                email: dto.email,
+            },
+        })
 
+        if(!user) throw new ForbiddenException("Incorrect credentials");
+
+        //compare passwords 
+        const pwMatches = await argon.verify(user.hash, dto.password);
+
+        if(!pwMatches) throw new ForbiddenException("Incorrect credentials");
+        //return user
+        delete user.hash;
+        return user;
     }
 
-    signup(){
-        
+    async signup(dto: AuthDto) {
+        //generate passwrod hash
+        const hash = await argon.hash(dto.password);
+
+        //save new user
+        try {
+            const user = await this.prisma.user.create({
+                data: {
+                    email: dto.email,
+                    hash,
+                }
+            });
+            delete user.hash;
+            return user;
+        } catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                if (error.code == 'P2002') {
+                    throw new ForbiddenException("Credentials taken");
+                }
+            } else {
+                throw error;
+            }
+        }
+
+        //return saved user
+
     }
 }
