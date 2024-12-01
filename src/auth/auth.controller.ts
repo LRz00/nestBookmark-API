c9e4640d152fda3a8c1f97d3cfaa995def4d33f5
Lara Rodrigues <lararods73@gmail.com>
2024-12-01 13:31:59 -0300

    finishing security configs

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Req } from "@nestjs/common";
+import { Body, Controller, HttpCode, HttpStatus, Post, Req } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { AuthDto } from "./dto";
 
@@ -10,12 +10,14 @@ export class AuthController{
     //this is instance creation via dependency injection
     constructor(private authService: AuthService){}
 
+ 
     @Post('signup')
     signup(@Body() dto: AuthDto){
 
         return this.authService.signup(dto);
     }
 
+    @HttpCode(HttpStatus.OK)
     @Post('signin')
     signin(@Body() dto: AuthDto){
         return this.authService.signin(dto);
